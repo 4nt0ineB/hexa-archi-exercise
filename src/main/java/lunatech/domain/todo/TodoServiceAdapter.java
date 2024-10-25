c8188fed2468fb87cce8ec5f897791e8d93ce935
Antoine Bastos <antoine.bastos@lunatech.fr>
2024-10-25 15:33:17 +0200

    added logging in todo service

--- src/main/java/lunatech/domain/todo/TodoServiceAdapter.java
+++ src/main/java/lunatech/domain/todo/TodoServiceAdapter.java
@@ -4,6 +4,7 @@ import io.vavr.control.Either;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import lunatech.domain.PermissionManager;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +12,8 @@ import java.util.UUID;
 
 public class TodoServiceAdapter implements TodoServicePort {
 
+    private static final Logger logger = Logger.getLogger(TodoServiceAdapter.class);
     private final Validator validator;
-
     private final TodoRepositoryPort todoRepository;
     private final PermissionManager permissionManager;
 
@@ -28,20 +29,23 @@ public class TodoServiceAdapter implements TodoServicePort {
 
     @Override
     public Either<String, List<Todo>> find(String origin, String target) {
+        logAction("Finding all todos", target, origin);
         return permissionManager.canSee(origin, target)
                 .map(__ -> todoRepository.find(target));
     }
 
     @Override
     public Either<String, List<Todo>> findWithTags(String origin, String target, List<String> tags) {
+        logAction("Finding todos with tags %s", target, origin, tags);
         return permissionManager.canSee(origin, target)
                 .map(__ -> todoRepository.findWithTags(target, tags));
     }
 
     @Override
-    public Either<String, Todo> findById(String origin, String username, UUID id) {
-        return permissionManager.canSee(origin, username)
-                .flatMap(__ -> todoRepository.findById(username, id)
+    public Either<String, Todo> findById(String origin, String target, UUID id) {
+        logAction("Finding todo ID: %s", origin, target, id);
+        return permissionManager.canSee(origin, target)
+                .flatMap(__ -> todoRepository.findById(target, id)
                         .map(Either::<String, Todo>right)
                         .orElse(Either.left("Todo not found"))
                 );
@@ -49,20 +53,23 @@ public class TodoServiceAdapter implements TodoServicePort {
 
     @Override
     public Either<String, Todo> update(String origin, String username, Todo todo) {
+        logAction("Updating todo: %s", origin, username, todo);
         var errors = validate(todo);
-        return errors.<Either<String, Todo>>map(Either::left).orElseGet(() -> permissionManager.canSee(origin, username)
-                .flatMap(__ ->
-                        todoRepository.update(username, todo)
+        return errors.<Either<String, Todo>>map(Either::left)
+                .orElseGet(() -> permissionManager.canSee(origin, username)
+                        .flatMap(__ -> todoRepository.update(username, todo)
                                 .map(Either::<String, Todo>right)
                                 .orElse(Either.left("Todo not found"))));
     }
 
     @Override
     public Either<String, Todo> add(String origin, String target, TodoDTO todoDto) {
+        logAction("Adding new todo:", origin, target, todoDto);
         var errors = validate(todoDto);
-        return errors.<Either<String, Todo>>map(Either::left).orElseGet(() -> permissionManager.canSee(origin, target)
-                .flatMap(__ -> {
-                    var todo = new Todo(
+        return errors.<Either<String, Todo>>map(Either::left)
+                .orElseGet(() -> permissionManager.canSee(origin, target)
+                        .flatMap(__ -> {
+                            var todo = new Todo(
                             UUID.randomUUID(),
                             todoDto.title(),
                             todoDto.description(),
@@ -75,6 +82,7 @@ public class TodoServiceAdapter implements TodoServicePort {
 
     @Override
     public Either<String, UUID> delete(String origin, String target, UUID id) {
+        logAction("Deleting todo ID: %s", origin, target, id);
         return permissionManager.canSee(origin, target)
                 .flatMap(__ -> todoRepository.delete(target, id)
                                 .map(Either::<String, UUID>right)
@@ -84,9 +92,15 @@ public class TodoServiceAdapter implements TodoServicePort {
     private <T> Optional<String> validate(T todo) {
         var violations = validator.validate(todo);
         if (!violations.isEmpty()) {
-            var messages = violations.stream().map(ConstraintViolation::getMessage);
-            return Optional.of(String.join(", ", messages.toList()));
+            var messages = String.join(", ", violations.stream().map(ConstraintViolation::getMessage).toList());
+            logger.warnf("Validation errors: %s", messages);
+            return Optional.of(messages);
         }
         return Optional.empty();
+    }
+
+    private void logAction(String msg, String origin, String target, Object... args) {
+        var prefix = "(%s" + (origin.equals(target) ? "" : " as %s") + ") ";
+        logger.infof(prefix + msg, origin, target, args);
     }
 }
